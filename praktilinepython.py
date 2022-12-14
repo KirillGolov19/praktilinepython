6b3f1ca0c6077e0585d0b58e1230182dab15af34
opilane <opilane@HP-CZC6297B9H>
2022-12-14 15:38:20 +0200

    Add project files.

---
+++ praktilinepython.py
@@ -0,0 +1,16 @@
+﻿from math import *
+from random import *
+try:
+    a=int(input("Sisestage number: "))
+    if(a>0):
+        print("Positiivne arv")
+    else:
+        print("Negatiivne arv")
+    if a%2==0:
+        print(f"{a} on paaris")
+    else:
+        print(f"{a} on paaritu")
+except:
+    print("Vale andmetüüp")
+
+#
